620d042b6aaa71c0025eafcfbf01b54139c55a0f
MCJBYDAY <jacobsmichaelc@gmail.com>
2022-06-09 12:57:37 -0500

    major overhaul of styling

--- client/src/pages/BiteSiteCreator.js
+++ client/src/pages/BiteSiteCreator.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useMutation } from '@apollo/client';
-// import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
+
 import Auth from '../utils/auth';
-// import { searchGoogleSoMes } from '../utils/queries';
+
 import { SAVE_BITESITE } from '../utils/mutations';
 // import { saveSoMeIds, getSavedSoMeIds } from '../utils/localStorage';
 
@@ -62,56 +62,76 @@ const BiteSiteCreator = () => {
 
   return (
     <>
-    <div role="group">
-    <h2 class="myTitleForUserClass" id="SoMeEmbedSelect">Which social would you like to embed?</h2>
-    <p class="myInstructionsToUserClass" id="SoMeEmbedSelectInstruct">Select one SoMe to embed. (required)</p>
-    <div class="grid grid-rows-3 grid-flow-col gap-5">
-          <div class="p-3 rounded-lg shadow-lg bg-pink-500">
-              <input class="ui-input__checkboxBlock"
-                  id="mySoMe1"
-                  name="mySoMe1_Unknown" type="checkbox" value=""></input>
-              <label class="myClassLabel"
-                  for="mySoMe1" >mySoMe1</label>
-          </div>
-          <div class="p-3 rounded-lg shadow-lg bg-pink-500">
-              <input class="ui-input__checkboxBlock"
-                  id="mySoMe2"
-                  name="mySoMe2_Unknown" type="checkbox" value=""></input>
-                  <label class="myClassLabel"
-                  for="mySoMe2" >mySoMe2</label>
-          </div>
-          <div class="p-3 rounded-lg shadow-lg bg-pink-500">
-              <input class="ui-input__checkboxBlock" id="mySoMe3"
-                  name="mySoMe3_Unknown" type="checkbox" value=""></input>
-                  <label class="myClassLabel"
-                  for="mySoMe3" >mySoMe3</label>
-          </div>
-          <div class="p-3 rounded-lg shadow-lg bg-pink-500">
-              <input class="ui-input__checkboxBlock"
-                  id="mySoMe1"
-                  name="mySoMe1_Unknown" type="checkbox" value=""></input>
-              <label class="myClassLabel"
-                  for="mySoMe1" >mySoMe1</label>
-          </div>
-          <div class="p-3 rounded-lg shadow-lg bg-pink-500">
-              <input class="ui-input__checkboxBlock"
-                  id="mySoMe2"
-                  name="mySoMe2_Unknown" type="checkbox" value=""></input>
-                  <label class="myClassLabel"
-                  for="mySoMe2" >mySoMe2</label>
-          </div>
-          <div class="p-3 rounded-lg shadow-lg bg-pink-500">
-              <input class="ui-input__checkboxBlock" id="mySoMe3"
-                  name="mySoMe3_Unknown" type="checkbox" value=""></input>
-                  <label class="myClassLabel"
-                  for="mySoMe3" >mySoMe3</label>
-          </div>
+    <div role="group" className="max-w-sm" id="myPageBoundary" >
+        <h2 className="myTitleForUserClass" id="SoMeEmbedSelect">Which social would you like to embed?</h2>
+        <p className="myInstructionsToUserClass" id="SoMeEmbedSelectInstruct">Select one SoMe to embed. (required)</p>
+        <div className="grid grid-rows-3 grid-cols-3 grid-flow-col gap-5">
+                <div className="p-3 rounded-lg shadow-lg bg-pink-500">
+                    <input className="ui-input__checkboxBlock"
+                        id="mySoMe1"
+                        name="mySoMe1_Unknown" type="checkbox" value=""></input>
+                    <label className="myClassLabel"
+                        for="mySoMe1" >mySoMe1</label>
+                </div>
+                <div className="p-3 rounded-lg shadow-lg bg-pink-500">
+                    <input className="ui-input__checkboxBlock"
+                        id="mySoMe2"
+                        name="mySoMe2_Unknown" type="checkbox" value=""></input>
+                        <label className="myClassLabel"
+                        for="mySoMe2" >mySoMe2</label>
+                </div>
+                <div className="p-3 rounded-lg shadow-lg bg-pink-500">
+                    <input className="ui-input__checkboxBlock" id="mySoMe3"
+                        name="mySoMe3_Unknown" type="checkbox" value=""></input>
+                        <label className="myClassLabel"
+                        for="mySoMe3" >mySoMe3</label>
+                </div>
+                <div className="p-3 rounded-lg shadow-lg bg-pink-500">
+                    <input className="ui-input__checkboxBlock"
+                        id="mySoMe1"
+                        name="mySoMe1_Unknown" type="checkbox" value=""></input>
+                    <label className="myClassLabel"
+                        for="mySoMe1" >mySoMe1</label>
+                </div>
+                <div className="p-3 rounded-lg shadow-lg bg-pink-500">
+                    <input className="ui-input__checkboxBlock"
+                        id="mySoMe2"
+                        name="mySoMe2_Unknown" type="checkbox" value=""></input>
+                        <label className="myClassLabel"
+                        for="mySoMe2" >mySoMe2</label>
+                </div>
+                <div className="p-3 rounded-lg shadow-lg bg-pink-500">
+                    <input className="ui-input__checkboxBlock" id="mySoMe3"
+                        name="mySoMe3_Unknown" type="checkbox" value=""></input>
+                        <label className="myClassLabel"
+                        for="mySoMe3" >mySoMe3</label>
+                </div>
+                <div className="p-3 rounded-lg shadow-lg bg-pink-500">
+                    <input className="ui-input__checkboxBlock" id="mySoMe3"
+                        name="mySoMe3_Unknown" type="checkbox" value=""></input>
+                        <label className="myClassLabel"
+                        for="mySoMe3" >mySoMe3</label>
+                </div>
+                <div className="p-3 rounded-lg shadow-lg bg-pink-500">
+                    <input className="ui-input__checkboxBlock"
+                        id="mySoMe1"
+                        name="mySoMe1_Unknown" type="checkbox" value=""></input>
+                    <label className="myClassLabel"
+                        for="mySoMe1" >mySoMe1</label>
+                </div>
+                <div className="p-3 rounded-lg shadow-lg bg-pink-500">
+                    <input className="ui-input__checkboxBlock"
+                        id="mySoMe2"
+                        name="mySoMe2_Unknown" type="checkbox" value=""></input>
+                        <label className="myClassLabel"
+                        for="mySoMe2" >mySoMe2</label>
+                </div>
+        </div>        
     </div>
-</div>
   {/* <!-- perhaps consider putting an icon on the button conveying that the user should advance --> */}
   {/* <button aria-label="Proceed to step 2" disabled=""
-    class="myButtonAdvanceClass">
-    <span class="myPutAnArrowHere"></span></button> */}
+    className="myButtonAdvanceClass">
+    <span className="myPutAnArrowHere"></span></button> */}
     </>
   );
 };
