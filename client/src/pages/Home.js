4f1e164c429bed6e991a5354331df144208530e9
Alec Dziwanowski <adziwanowski@gmail.com>
2022-06-13 11:33:39 -0400

    mobile edits to landing page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useMutation } from '@apollo/client';
-import SignUpForm from '../components/SignupForm'
-import LoginForm from '../components/LoginForm'
 import Navbar from '../components/Navbar'
-import Auth from '../utils/auth';
-import script from '../utils/script';
-// import { saveSoMeIds, getSavedSoMeIds } from '../utils/localStorage';
 
 const Home = () => {
   // create state for holding returned google api data
@@ -14,7 +8,7 @@ const Home = () => {
   return (
     <>
       <section id="profile" className="background flex flex-col text-center">
-        <h1 className="text-6xl">B I T E S I T E</h1>
+        <h1 className="text-6xl specialHeader">B I T E S I T E</h1>
         <img className="rounded-full my-10 mx-auto" src="https://bitesites.net/assets/favicon.png"></img>
         <Navbar />
       </section>
